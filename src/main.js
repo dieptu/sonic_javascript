2c0f74be82c565b0bbf65263b288f69c54449c79
tuminhdiep <tuminh2307@gmail.com>
2025-05-12 21:53:52 -0400

    infinite background and platform running

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import k from "./kaplayCtx";
+import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
 k.loadSprite("platforms", "graphics/platforms.png");
@@ -54,3 +55,10 @@ k.loadSound("ring", "sounds/Ring.wav");
 k.loadSound("city", "sounds/city.mp3");
 
 
+k.scene("main-menu", mainMenu);
+
+k.scene("game", ()=>{});
+
+k.scene("gameover", ()=>{});
+
+k.go("main-menu");
